fe646627dc0916ea504d1f465f812d767bad28e7
UcheSylvester <okorocode@gmail.com>
2019-10-18 21:41:52 +0100

    basic app setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieThumbnailComponent } from './movies/movie-thumbnail/movie-thumbnail.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
+import { ErrorsComponent } from './errors/errors.component';
+import { FavoritesComponent } from './movies/favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoviesComponent,
+    MovieThumbnailComponent,
+    MovieDetailsComponent,
+    ErrorsComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule
